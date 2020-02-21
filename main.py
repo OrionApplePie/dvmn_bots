60a9216e0cfa2fb11216554389b9cf7c6412b915
Alexander Ten <orionapplepie@gmail.com>
2020-02-22 09:56:08 +1000

    Улучшен README, добавлена проверка Http статуса и задержка переподключения

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
+from textwrap import dedent
+from time import sleep
 
 import requests
 import telegram
 from dotenv import find_dotenv, load_dotenv
 from requests.compat import urljoin
-from requests.exceptions import ConnectionError, ReadTimeout
+from requests.exceptions import ConnectionError, HTTPError, ReadTimeout
 
 DEVMAN_BASE_URL = "https://dvmn.org/"
 
@@ -12,12 +14,15 @@ DEVMAN_API_BASE_URL = "https://dvmn.org/api/"
 DEVMAN_REVIEWS_URL = "user_reviews"
 DEVMAN_LONG_POLLING_URL = "long_polling"
 DEVMAN_TIMEOUT = 120
-CHAT_ID = 472955649
+
+FAIL_ATTEMPTS_COUNT = 10
+SLEEP_TIME = 60 * 2
 
 
 def main():
     devman_token = os.getenv("DEVMAN_API_TOKEN")
     telegram_token = os.getenv("TELEGRAM_API_TOKEN")
+    telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
     headers = {
         "Authorization": f"Token {devman_token}",
@@ -26,9 +31,11 @@ def main():
     params = {}
     url = urljoin(DEVMAN_API_BASE_URL, DEVMAN_LONG_POLLING_URL)
 
-    # настройки прокси берет из переменной окружения
+    # настройки прокси берутся из переменной окружения
     req = telegram.utils.request.Request()
     bot = telegram.Bot(token=telegram_token, request=req)
+
+    fail_count = 0
 
     while True:
         try:
@@ -38,19 +45,30 @@ def main():
                 params=params,
                 timeout=DEVMAN_TIMEOUT,
             )
+            response.raise_for_status()
+
+        except HTTPError as error:
+            print(f"Http error, try again: \n{error}\n")
+            continue
+
         except ReadTimeout as error:
             print(f"Timeout error, try again:\n{error}\n")
             continue
 
         except ConnectionError as error:
-            print(f"Connection error, try again:\n{error}\n")
+            fail_count += 1
+            if fail_count >= FAIL_ATTEMPTS_COUNT:
+                print(f"Too much connection attempts fail, waiting {SLEEP_TIME} secs.\n{error}\n")
+                sleep(SLEEP_TIME)
+            else:
+                print(f"Connection error, try again:\n{error}\n")
             continue
 
+        fail_count = 0
         resp_data = response.json()
 
         if resp_data["status"] == "timeout":
             params["timestamp"] = resp_data["timestamp_to_request"]
-            print(resp_data)
 
         if resp_data["status"] == "found":
             messages = []
@@ -63,23 +81,22 @@ def main():
                 )
                 lesson_title = attempt["lesson_title"]
                 lesson_url = attempt["lesson_url"]
-                message = "".join([
-                    "У Вас проверили работу",
-                    " ",
-                    f"\u00AB{lesson_title}\u00BB",
-                    "\n\n",
-                    attempt_result,
-                    "\n\n",
-                    urljoin(DEVMAN_BASE_URL, lesson_url)
-                ])
-                messages.append(message)
+
+                message = f"""
+                У Вас проверили работу \u00AB{lesson_title}\u00BB
+
+                {attempt_result}
+
+                {urljoin(DEVMAN_BASE_URL, lesson_url)}
+                """
+
+                messages.append(dedent(message))
             for message in messages:
                 bot.send_message(
-                    chat_id=CHAT_ID,
+                    chat_id=telegram_chat_id,
                     text=message
                 )
             params["timestamp"] = resp_data["last_attempt_timestamp"]
-            print(resp_data)
 
 
 if __name__ == "__main__":
